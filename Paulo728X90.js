8fbd982719e86b422e9f15ab306b36cc2d27afd6
firmanriawan9 <31408277+firmanriawan9@users.noreply.github.com>
2017-11-14 19:14:05 +0700

    Add files via upload

---
+++ Paulo728X90.js
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="//effective-ads.com/display/js/ads.js?13303&2023&728&90&0"></script>
+    <div class="infoads">
+        <a href="http://www.androedland.net/p/advertise.html" target="_blank" title="Ads By Androedland"><p>Ads By ADL</p></a>
+    </div>
